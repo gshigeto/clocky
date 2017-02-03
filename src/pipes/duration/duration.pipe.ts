d483358db0156027142a549a0028e0bc7c0fb32f
Gavin Nitta <gshigeto@gmail.com>
2017-02-03 16:03:16 -0700

    Fix duration for seconds

--- src/pipes/duration/duration.pipe.ts
+++ src/pipes/duration/duration.pipe.ts
@@ -19,7 +19,7 @@ export class Duration {
     if (shift.clockOut) {
       let hrs = Math.floor((parseInt(shift.clockOut) - parseInt(shift.clockIn)) / 3600000);
       let mins = Math.floor(((parseInt(shift.clockOut) - parseInt(shift.clockIn)) / 60000) % 60);
-      let secs = Math.floor(((parseInt(shift.clockOut) - parseInt(shift.clockIn)) / 1000) % 60);
+      let secs = Math.round(((parseInt(shift.clockOut) - parseInt(shift.clockIn)) / 1000) % 60);
       return `${hrs}H ${mins}M ${secs}S`;
     }
     return 'MISSING OUT';
